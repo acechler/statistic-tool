49f2659500145037a15c3195b9f04b69b086415c
Aaron Echler <acechler@gmail.com>
2024-05-27 09:18:37 -0400

    Create a probability bag.

--- index.js
+++ index.js
@@ -2,30 +2,28 @@
 const probability_bag = {
     
     name : "probability bag",
-    items : [],
-    loop(){
-        this.items.forEach((item)=>{
-            console.log(item);
-        });
+    items : {},
+    addEntry(name, value) {
+        this.items[name] = value;
     },
-
     calc(){
-        let accumulator = 0;
-        this.items.forEach((item) =>{
-            console.log(accumulator);
-            item *= accumulator;
-        });
-        return accumulator;
+        let product = 1;
+        for(let key in this.items){
+            if(this.items.hasOwnProperty(key) && typeof this.items[key] === 'number'){
+                product *= this.items[key];
+            }
+        }
+        return product;
     }
 
 };
 
 let chance_on_counter = 1/2;
 let chance_on_rolling_6 = 1/6;
-//console.log(chance_on_counter * chance_on_rolling_6);
 
+probability_bag.addEntry("chance of dice being on counter", chance_on_counter);
+probability_bag.addEntry("chance of rolling six sided dice", chance_on_rolling_6);
 
-probability_bag.items.push(chance_on_counter);
-probability_bag.items.push(chance_on_rolling_6);
+console.log(probability_bag);
 
 console.log(probability_bag.calc());
